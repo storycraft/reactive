5aea6f5a23c607dffedc417e0875b048150c3772
storycraft <storycraft@pancake.sh>
2025-02-08 15:56:39 +0900

    feat: separate load method

--- crates/main/src/resource.rs
+++ crates/main/src/resource.rs
@@ -6,36 +6,35 @@ use reactivity::binding::Binding;
 use crate::{event_loop::context::AppCx, state::StateCell};
 
 pub struct Resource<T> {
-    status: Pin<Rc<StateCell<Option<T>>>>,
+    state: Pin<Rc<StateCell<Option<T>>>>,
 }
 
 impl<T> Resource<T> {
-    pub fn of<Fut>(f: impl FnOnce() -> Fut) -> Self
+    pub fn new() -> Self {
+        Self {
+            state: Rc::pin(StateCell::new(None)),
+        }
+    }
+
+    pub fn load<Fut>(&self, fut: Fut)
     where
         Fut: Future<Output = T> + 'static,
         T: 'static,
     {
-        let fut = f();
-        let status = Rc::pin(StateCell::new(None));
+        AppCx::with(|cx| {
+            let state = self.state.clone();
 
-        if AppCx::is_set() {
-            AppCx::with(|cx| {
-                cx.executor()
-                    .spawn({
-                        let status = status.clone();
-
-                        async move {
-                            status.as_ref().set(Some(fut.await));
-                        }
-                    })
-                    .detach();
-            });
-        }
-
-        Self { status }
+            cx.executor()
+                .spawn({
+                    async move {
+                        state.as_ref().set(Some(fut.await));
+                    }
+                })
+                .detach();
+        });
     }
 
     pub fn get(&self, binding: Pin<&Binding>) -> Option<T> {
-        self.status.as_ref().take_get(binding)
+        self.state.as_ref().take_get(binding)
     }
 }
