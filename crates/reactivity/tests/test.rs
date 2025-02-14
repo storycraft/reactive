3f6e41a3cdcdbe95ad9b3be3c3001c0b42d5a557
storycraft <storycraft@pancake.sh>
2025-02-14 15:03:20 +0900

    feat: update test

--- crates/reactivity/tests/test.rs
+++ crates/reactivity/tests/test.rs
@@ -1,29 +1,71 @@
-// use core::{
-//     future::{poll_fn, Future},
-//     pin::{pin, Pin},
-// };
+use core::{
+    cell::Cell,
+    future::{poll_fn, Future},
+    pin::{pin, Pin},
+};
 
-// use reactivity::{let_effect, queue::Queue, tracker::DependencyTracker};
+use pin_project::pin_project;
+use reactivity::{effect::Binding, let_effect, queue::Queue, tracker::DependencyTracker};
+use scoped_tls_hkt::scoped_thread_local;
 
-// fn run_system<'a, Fut: Future + 'a>(f: impl FnOnce(Pin<&'a Queue>) -> Fut) -> Fut::Output {
-//     let mut queue = pin!(Queue::new());
-//     let mut fut = pin!(f(queue.as_ref()));
+scoped_thread_local!(static QUEUE: for<'a> Pin<&'a Queue>);
 
-//     pollster::block_on(poll_fn(|cx| queue.(fut.as_mut(), cx)))
-// }
+fn run_system<Fut: Future>(fut: Fut) -> Fut::Output {
+    let queue = pin!(Queue::new(None));
+    let queue = queue.as_ref();
+    let mut fut = pin!(fut);
 
-// #[test]
-// fn effects() {
-//     run_system(async {
-//         let a = pin!(DependencyTracker::new());
-//         let a = a.into_ref();
-//         let b = pin!(StateCell::new(0));
-//         let b = b.into_ref();
+    pollster::block_on(poll_fn(|cx| {
+        QUEUE.set(queue, || {
+            let v = fut.as_mut().poll(cx);
+            queue.run(cx.waker());
+            v
+        })
+    }))
+}
 
-//         let_effect!(|| {
-//             b.set(a.get($) + 1);
-//         });
+#[derive(derive_more::Debug)]
+#[pin_project]
+pub struct State<T: ?Sized> {
+    #[pin]
+    tracker: DependencyTracker,
+    #[debug(skip)]
+    value: Cell<T>,
+}
 
-//         a.update(|a| a + 1);
-//     });
-// }
+impl<T> State<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            tracker: DependencyTracker::new(),
+            value: Cell::new(value),
+        }
+    }
+
+    #[inline]
+    pub fn set(self: Pin<&Self>, value: T) {
+        self.value.set(value);
+        QUEUE.with(|queue| self.project_ref().tracker.notify(queue));
+    }
+}
+
+impl<T: Copy> State<T> {
+    #[inline]
+    pub fn get(self: Pin<&Self>, binding: Binding) -> T {
+        self.project_ref().tracker.register(binding);
+        self.value.get()
+    }
+}
+
+#[test]
+fn effects() {
+    run_system(async {
+        let a = pin!(State::new(2));
+        let a = a.into_ref();
+        let b = pin!(State::new(0));
+        let b = b.into_ref();
+
+        let_effect!(b.set(a.get($) + 1));
+
+        a.set(50);
+    });
+}
