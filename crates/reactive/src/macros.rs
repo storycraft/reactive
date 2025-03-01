1d880ce56210820457870ddcf7d008b10b6a707c
storycraft <storycraft@pancake.sh>
2025-03-01 15:16:20 +0900

    doc: add doc

--- crates/reactive/src/macros.rs
+++ crates/reactive/src/macros.rs
@@ -1,4 +1,6 @@
 #[macro_export]
+/// Take ownership of the variable and pin reference locally.
+/// Returns pinned immutable reference.
 macro_rules! pin_ref {
     ($name:ident) => {
         let $name = ::core::pin::pin!($name);
