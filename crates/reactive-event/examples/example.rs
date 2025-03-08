771ef74af8b3a83278312811064e2fe11330928a
storycraft <storycraft@pancake.sh>
2025-03-09 02:16:45 +0900

    feat: move to example

--- crates/reactive-event/examples/example.rs
+++ crates/reactive-event/examples/example.rs
@@ -2,8 +2,7 @@ use core::pin::pin;
 
 use reactive_event::{EventTarget, Listener};
 
-#[test]
-fn test() {
+fn main() {
     let target = pin!(<EventTarget!(&mut i32)>::new());
     let target = target.into_ref();
 
@@ -11,10 +10,11 @@ fn test() {
         let mut b = 2;
         let listener = pin!(Listener::new(|a: &mut i32| {
             dbg!(*a + b);
+            *a += 1;
             b += 2;
             false
         }));
-        target.bind(listener.as_ref());
+        target.bind(listener);
 
         let listener2 = pin!(Listener::new(|a: &mut i32| {
             dbg!(*a + 5);
@@ -24,4 +24,7 @@ fn test() {
 
         target.emit_mut(&mut 2);
     }
+
+    // nothing happens because all listeners are dropped
+    target.emit_mut(&mut 0);
 }
