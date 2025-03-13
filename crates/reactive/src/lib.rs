af956835b6e618d0ffb76d7546e0b203c28243ff
storycraft <storycraft@pancake.sh>
2025-03-14 00:56:19 +0900

    feat: remove WithChild, rename element to div

--- crates/reactive/src/lib.rs
+++ crates/reactive/src/lib.rs
@@ -8,7 +8,7 @@ pub use reactivity_winit::winit;
 pub use skia_safe;
 pub use taffy;
 
-use core::future::{Future, pending};
+use core::future::{pending, Future};
 use reactive_tree::element::Element;
 use scopeguard::defer;
 use taffy::Style;
@@ -44,47 +44,7 @@ impl SetupFn for () {
     }
 }
 
-/// Representation of a functional component with a child
-pub trait WithChild<Child> {
-    type Output;
-
-    fn child(self, child: Child) -> impl SetupFn<Output = Self::Output>;
-}
-
-impl<F, Child, Fut> WithChild<Child> for F
-where
-    F: FnOnce(Ui, Child) -> Fut,
-    Fut: Future,
-{
-    type Output = Fut::Output;
-
-    fn child(self, child: Child) -> impl SetupFn<Output = Fut::Output> {
-        |ui| self(ui, child)
-    }
-}
-
-#[easy_ext::ext(SetupFnWithChildExt)]
-pub impl<F> F
-where
-    F: WithChild<()>,
-{
-    fn show(self, ui: Ui) -> impl Future<Output = F::Output> {
-        self.child(()).show(ui)
-    }
-}
-
-pub fn create_element<F: SetupFn>(style: Style, f: F) -> impl SetupFn<Output = F::Output> {
-    |ui: Ui| async move {
-        let id = ui.append(Element::new(style)).unwrap();
-        defer!({
-            _ = ui.remove(id);
-        });
-
-        f.show(ui.sub_ui(id)).await
-    }
-}
-
-pub fn element<F: SetupFn>(f: F) -> impl SetupFn<Output = F::Output> {
+pub fn div<F: SetupFn>(f: F) -> impl SetupFn<Output = F::Output> {
     |ui: Ui| async move {
         let id = ui.append(Element::new(Style::DEFAULT)).unwrap();
         defer!({
@@ -95,6 +55,3 @@ pub fn element<F: SetupFn>(f: F) -> impl SetupFn<Output = F::Output> {
     }
 }
 
-pub fn styled(style: Style) {
-
-}
