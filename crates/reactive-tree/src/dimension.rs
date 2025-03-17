577c76e7bf5677dfb39b1dcf266d5a2e47929ae9
storycraft <storycraft@pancake.sh>
2025-03-17 22:11:32 +0900

    feat: remove Auto, add Default

--- crates/reactive-tree/src/dimension.rs
+++ crates/reactive-tree/src/dimension.rs
@@ -1,17 +1,20 @@
-#[derive(Debug, Clone, Default, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Dimension {
     Absolute(f32),
     Percent(f32),
-    #[default]
-    Auto,
 }
 
 impl Dimension {
-    pub fn resolve(self, v: f32) -> Option<f32> {
+    pub fn resolve(self, v: f32) -> f32 {
         match self {
-            Dimension::Absolute(absolute) => Some(absolute),
-            Dimension::Percent(p) => Some(p * v),
-            Dimension::Auto => None,
+            Dimension::Absolute(absolute) => absolute,
+            Dimension::Percent(p) => p * v,
         }
+    }
+}
+
+impl Default for Dimension {
+    fn default() -> Self {
+        Self::Absolute(0.0)
     }
 }
